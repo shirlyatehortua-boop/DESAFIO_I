e617b1cd1e2dfe5cd84ddffe4a03fd5b78a4b3b9
unknown <shirly.atehortua@udea.edu.co>
2026-09-16 00:10:08 -0500

    Representacion del tablero

--- Code/DESAFIO_I/TABLERO.h
+++ Code/DESAFIO_I/TABLERO.h
@@ -3,3 +3,30 @@
 
 #endif // TABLERO_H
 //ESTE HEADER LO UTILIZARE PARA EL TABLERO PARA TENER MAYOR ORGANIZACION
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int filas, columnas;
+
+    cout << "Ingrese el numero de filas: ";
+    cin >> filas;
+
+    cout << "Ingrese el numero de columnas: ";
+    cin >> columnas;
+
+    vector<vector<int>> tablero(filas, vector<int>(columnas, 0));
+
+    // Mostrar el tablero
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout << tablero[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
