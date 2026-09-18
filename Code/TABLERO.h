8d84635fd63355aef06e3ab2e30f1d6ef0ee7743
unknown <shirly.atehortua@udea.edu.co>
2026-09-17 22:34:31 -0500

    Creacion de tablero parte 1, inicio nuevo

--- Code/TABLERO.h
+++ Code/TABLERO.h
@@ -1,4 +1,21 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-#endif // TABLERO_H
+//Se inicia de nuevo por motivo de error en guia
+// Estaba haciendo el proyecto del semestre pasado
+//Entonces para seguir con el orden de los commits retomo nuevamente con los codigos
+
+
+    unsigned char* crearTablero(int ancho, int alto);
+
+void inicializarTablero(unsigned char* tablero, int ancho, int alto);
+
+void mostrarTablero(unsigned char* tablero, int ancho, int alto);
+
+void liberarTablero(unsigned char* tablero);
+
+int obtenerFicha(unsigned char* tablero, int ancho, int fila, int columna);
+
+void modificarFicha(unsigned char* tablero, int ancho, int fila, int columna, int ficha);
+
+#endif
